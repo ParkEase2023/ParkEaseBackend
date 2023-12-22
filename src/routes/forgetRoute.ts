ca9f7e24fb537e348d7a8c1f85a2df32c6d6ce17
Surawee kraikruan <67056659+sura1542@users.noreply.github.com>
2023-12-22 20:09:17 +0700

    commit 20.09

--- src/routes/forgetRoute.ts
+++ src/routes/forgetRoute.ts
@@ -2,9 +2,7 @@ import express from 'express';
 import { forgetpassword } from '../controllers/forgetpasswordController.js';
 
 const app = express();
-
 const router = express.Router();
-
 router.post('/forgetpassword', forgetpassword);
 
 
