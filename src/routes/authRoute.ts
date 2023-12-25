27df5d0a95fe2252cac961dedf24514faae0ceee
thanakrit410 <scms.dev01@gmail.com>
2023-12-25 21:28:53 +0700

    commit 21.28

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { generateOTP } from '../controllers/otpController';
 
-const app = express();
+
 
 const router = express.Router();
 router.get('/generateOTP', generateOTP);
