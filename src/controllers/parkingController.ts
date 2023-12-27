06863aa480f93c2bc7837ed7e7e19e8552897cbd
thanakrit410 <scms.dev01@gmail.com>
2023-12-27 14:07:44 +0700

    commit 14.07

--- src/controllers/parkingController.ts
+++ src/controllers/parkingController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
 
-const apiKey = 'AIzaSyBUc-PE0AR_DrUauMTAcjbfqle2NpInGhM'; 
-const apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
+const apiKey = 'AIzaSyDWq2S-Qh6vwRbYnuvHHEZ4KLG-TsJvDsg'; 
+const apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?place_id=ChIJeRpOeF67j4AR9ydy_PIzPuM&key=AIzaSyDWq2S-Qh6vwRbYnuvHHEZ4KLG-TsJvDsg';
 
 export const getFormattedAddress = async (req: Request, res: Response) => {
     async function getFormattedAddress(latitude: number, longitude: number): Promise<void> {
@@ -11,6 +11,7 @@ export const getFormattedAddress = async (req: Request, res: Response) => {
           params: {
             latlng: `${latitude},${longitude}`,
             key: apiKey,
+            sensor:true
           },
         });
     
