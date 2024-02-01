2103ec0ff5efcfad91eb9f20ec2602a155191bc6
Palita Simaborvonsut <98012277+PattySoCute@users.noreply.github.com>
2024-02-01 11:49:23 +0700

    commit 11.49

--- src/controllers/mylistController.ts
+++ src/controllers/mylistController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import Mylist from '../models/Mylist';
+import mongoose from 'mongoose';
 
 export const addMyList = async (req: Request, res: Response) => {
     console.log('addMyList working');
@@ -9,3 +10,54 @@ export const addMyList = async (req: Request, res: Response) => {
         message: 'addMylist successfully',
     });
 };
+
+export const getMyList = async (req: Request, res: Response) => {
+    console.log('getMyList working');
+    const query = req.query;
+    console.log('getMyList: ', query.userId);
+    const regexQuery = query.userId;
+    console.log(regexQuery);
+    try {
+        if (query.userId !== '') {
+            const regexQuery = query.userId;
+            if (regexQuery) {
+                console.log(regexQuery);
+                const dataMyList: any = await Mylist.aggregate([
+                    { $match: { userId: new mongoose.Types.ObjectId(regexQuery.toString()) } },                    
+                        {
+                        $lookup: {
+                          from: "parkings",
+                          localField: "parkingId",
+                          foreignField: "_id",
+                          as: "myList"
+                        }
+                        },
+                      
+                    { $sort: { createdAt: -1 } },
+                ])
+                if (dataMyList.length > 0) {
+                    res.status(200).json({
+                        message: 'success',
+                        myList: dataMyList,
+                    });
+                    console.log(dataMyList);
+                } else {
+                    console.log('No data');
+                    res.status(200).json({
+                        message: 'not success',
+                        myList: dataMyList,
+                    });
+                }
+            } else {
+                console.log('regexQuery is undefined');
+                res.status(400).json({ message: 'Invalid query' });
+            }
+        } else {
+            console.log('No search');
+            res.status(400).json({ message: 'Please enter place name' });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500);
+    }
+}
