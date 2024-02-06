318537a49432653ab068f18e99b988c7d963397a
Palita Simaborvonsut <98012277+PattySoCute@users.noreply.github.com>
2024-02-06 13:08:34 +0700

    commit 13.08

--- src/routes/mylistRoute.ts
+++ src/routes/mylistRoute.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addMyList, getMyList } from '../controllers/mylistController';
+import { addMyList, deleteMyList, getMyList } from '../controllers/mylistController';
 
 
 const router = express.Router();
 router.post('/addmylist', addMyList);
 router.get('/getmylist', getMyList);
+router.delete('/deletemylist', deleteMyList);
 
 
 export default router;
