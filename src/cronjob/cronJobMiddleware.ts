4178a22b7d5f188c08482e70bd1aed709ae31f4a
thanakrit410 <scms.dev01@gmail.com>
2024-02-25 15:46:13 +0700

    commit 15.46

--- src/cronjob/cronJobMiddleware.ts
+++ src/cronjob/cronJobMiddleware.ts
@@ -5,9 +5,7 @@ import { cronJobApproveRecipien } from '../controllers/recipienController';
 
 export const cronJobMiddleware = () => {
     cron.schedule('0 * * * *', async () => {
-        console.log('Running a task every minute');
         try {
-            // Make an HTTP GET request to your controller route
             const response = await axios.get('http://localhost:4000/recipien/getAllRecipien');
             const task = response.data.data
             console.log('HTTP request successful');
@@ -19,7 +17,6 @@ export const cronJobMiddleware = () => {
         }
     });
 
-    // Schedule a task to run at 2:30 AM every day
     cron.schedule('30 2 * * *', () => {
         console.log('Running a task at 2:30 AM every day');
     });
--- src/routes/omiseRoute.ts
+++ src/routes/omiseRoute.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import {createdPromptPayQRCode, createdRecipient} from '../controllers/omiseController';
 import {CheckCharge} from '../controllers/omiseController'
-import {Recipient} from '../controllers/omiseController';
 import {Transfers} from '../controllers/omiseController';
 const router = express.Router();
 router.post('/created', createdPromptPayQRCode);
 router.post('/check',CheckCharge);
-router.get('/recipient',Recipient);
 router.get('/transfers',Transfers);
 router.post('/createrecipient',createdRecipient);
 
